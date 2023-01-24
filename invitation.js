c91df37644538461c209c950d77436aed332e547
Hector Diaz <hectordiaz1103@gmail.com>
2023-01-24 16:14:06 -0500

    styling

--- invitation.js
+++ invitation.js
@@ -27,7 +27,7 @@ $("#yes").on("click", function (event) {
 var parrNoGuest = '<p id="parrNo"><br>How many guests will be attending? Please refer to your text message to see number of guests invited.</br></p>';
 var howManyAttending = '<input id="howMany" placeholder="Enter #"></input>';
 var numberButton = '<button class="numberButton" id="numbutton" type="button">Confirm</button></a><br>'
-var emailForm ='<div class="emailForm"><form action="mailto:rosy@gmail.com" method="post" enctype="text/plain" class="bold">Name:<br><input type="text" name="Name" class="italic" placeholder="Name of Guest"><br><br>Enter Your first Meal Option:<br><input type="text" name="1st Meal Selection" class="italic" placeholder="Beef, Chicken, or Fish"><br><br>Enter Your second Meal Option:<br><input type="text" name="2nd Meal Option" class="italic" placeholder="Beef, Chicken, or Fish"><br><br><input type="submit" value="Send" class="respond"><input type="reset" value="Reset" class="respond"></form></div>'
+var emailForm ='<div class="emailForm"><form action="mailto:rosy@gmail.com" method="post" enctype="text/plain" class="bold">Name:<br><input type="text" name="Name" class="italic" placeholder="Name of Guest"><br><br>Enter Your first Meal Option:<br><input type="text" name="1st Meal Selection" class="italic" placeholder="Beef, Chicken, or Fish"><br><br>Enter Your second Meal Option:<br><input type="text" name="2nd Meal Option" class="italic" placeholder="Beef, Chicken, or Fish"><br><br><input type="submit" value="Send" class="respond"><input type="reset" value="Reset" class="respond reset"></form></div>'
 
 
 $("#yes").click(function(){
